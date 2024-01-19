82f95611cebe9f33c99a3354067179a0b1cb5455
S0M2 <143214620+S0M2@users.noreply.github.com>
2024-01-19 16:50:29 +0100

    update

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 // Configuration de CodeMirror pour le code HTML
 var htmlEditor = CodeMirror.fromTextArea(document.getElementById("html-code"), {
-    lineNumbers: true,
-    mode: "htmlmixed",
-    theme: "default",
-  });
+  lineNumbers: true,
+  mode: "htmlmixed",
+  theme: "dracula", // Remplacé "default" par "dracula"
+});
 
-  // Configuration de CodeMirror pour le code CSS
-  var cssEditor = CodeMirror.fromTextArea(document.getElementById("css-code"), {
-    lineNumbers: true,
-    mode: "css",
-    theme: "default",
-  });
+// Configuration de CodeMirror pour le code CSS
+var cssEditor = CodeMirror.fromTextArea(document.getElementById("css-code"), {
+  lineNumbers: true,
+  mode: "css",
+  theme: "dracula", // Remplacé "default" par "dracula"
+});
 
-  // Mettez à jour la page en fonction des modifications dans les éditeurs
-  htmlEditor.on("change", function () {
-    updatePreview();
-  });
+// Mettez à jour la page en fonction des modifications dans les éditeurs
+htmlEditor.on("change", function () {
+  updatePreview();
+});
 
-  cssEditor.on("change", function () {
-    updatePreview();
-  });
+cssEditor.on("change", function () {
+  updatePreview();
+});
 
-  // Fonction pour mettre à jour l'aperçu en direct
-  function updatePreview() {
-    var previewFrame = document.getElementById("preview");
-    var preview = previewFrame.contentDocument || previewFrame.contentWindow.document;
-    preview.open();
-    preview.write(htmlEditor.getValue() + '<style>' + cssEditor.getValue() + '</style>');
-    preview.close();
-  }
+// Fonction pour mettre à jour l'aperçu en direct
+function updatePreview() {
+  var previewFrame = document.getElementById("preview-frame");
+  var preview = previewFrame.contentDocument || previewFrame.contentWindow.document;
+  preview.open();
+  preview.write(htmlEditor.getValue() + '<style>' + cssEditor.getValue() + '</style>');
+  preview.close();
+}
