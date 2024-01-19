a44e60087cb0afedb83f11782300ef6c38628d13
S0M2 <143214620+S0M2@users.noreply.github.com>
2024-01-19 17:30:33 +0100

    test

--- script.js
+++ script.js
@@ -2,23 +2,14 @@
 var htmlEditor = CodeMirror.fromTextArea(document.getElementById("html-code"), {
   lineNumbers: true,
   mode: "htmlmixed",
-  theme: "dracula", // Remplacé "default" par "dracula"
+  theme: "dracula",
 });
 
 // Configuration de CodeMirror pour le code CSS
 var cssEditor = CodeMirror.fromTextArea(document.getElementById("css-code"), {
   lineNumbers: true,
   mode: "css",
-  theme: "dracula", // Remplacé "default" par "dracula"
-});
-
-// Mettez à jour la page en fonction des modifications dans les éditeurs
-htmlEditor.on("change", function () {
-  updatePreview();
-});
-
-cssEditor.on("change", function () {
-  updatePreview();
+  theme: "dracula",
 });
 
 // Fonction pour mettre à jour l'aperçu en direct
